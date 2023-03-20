9bbbcc21f6fb9b8b98254ccc38a3024301d24767
Yutao Meng <mengyutao@uniontech.com>
2023-03-20 15:35:26 +0800

    feat: Init repo
    Init repo

---
+++ debian/test.sh
@@ -0,0 +1,19 @@
+#!/bin/sh
+
+: "${SRCDIR:=./js/src}"
+: "${BUILDDIR:=./debian/build}"
+: "${DEB_HOST_ARCH:=$(dpkg --print-architecture)}"
+
+if "$BUILDDIR/js/src/js" -e 'print("Hello, world")'; then
+	echo "Smoke-test successful, continuing with full test suite"
+else
+	echo "Smoke-test failed: did interpreter initialization fail? (see #873778)"
+	exit 1
+fi
+
+if make -C "${BUILDDIR}" -k check-jstests; then
+	echo "check-jstests successful"
+else
+	echo "check-jstests failed"
+	exit 1
+fi
